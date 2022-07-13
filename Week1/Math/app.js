b699dda138fd22bee19c8a62a4211c753e091159
Green1-j <joegr193@gmail.com>
2022-07-13 14:08:56 -0400

    week1 work

--- Week1/Math/app.js
+++ Week1/Math/app.js
@@ -1,9 +1,8 @@
 const math = require('./math');
 
-// console.log('Math: ', math.addition(10, 20))
-// console.log('Math: ', math.subtraction(50, 30))
-// console.log('Math: ', math.multiplication(8, 8))
-// console.log('Math:', math.division(120, 4))
+ console.log('Math: ', math.addition(10, 20))
+ console.log('Math: ', math.subtraction(50, 30))
+ console.log('Math: ', math.multiplication(8, 8))
+ console.log('Math:', math.division(9, 3))
 
-console.log('Math: ', 2 +2);
-console.log('Math: ', math.addition(5, 4))
+console.log('Math: ', 2 +2)
