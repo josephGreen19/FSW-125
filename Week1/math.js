245eec43b373fb4b0584f95f7ca64bd5b8affbd6
Green1-j <joegr193@gmail.com>
2022-07-13 13:35:45 -0400

    week 1

--- Week1/math.js
+++ Week1/math.js
@@ -1,1 +1,20 @@
-git 
+const addition = (a, b) => {
+    return a + b
+}
+// const subtraction = (a, b) => {
+//     return a - b
+// }
+// const multiplication = (a, b) => {
+//     return a + b
+// }
+// const division = (a, b) => {
+//     return a / b
+//}
+
+module.exports = {
+    addition
+    // subtraction
+    // multiplication
+    // division
+
+}
